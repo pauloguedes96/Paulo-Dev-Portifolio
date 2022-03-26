efce513d64774ca7bea55ffc2689341e409dd19b
Paulo Guedes <cesarmota94@gmail.com>
2022-03-26 19:05:46 -0300

    c5

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,40 @@
+document.addEventListener('DOMContentLoaded', () => {
+    populaProjects();
+});
+
 let titleProjeto = document.querySelectorAll('.titleProjeto');
 let descProjeto = document.querySelectorAll('.descProjeto');
 let headerProjeto = document.querySelectorAll('.headerProjeto');
-let btnProjeto = document.querySelectorAll('.btnProjeto a');
+let btnProjeto = document.querySelectorAll('.btnProjeto');
 
 let projetos = [
     {
+        id:1231231,
         nome: "Landind Page Advogado Online",
         description: "Landing page desenvolvida para advogados, com intuito de captar clientes e passar mais confiança aos mesmos.",
         capa: "/assets/images/capa-1.jpg",
-        /*         link:"https://pauloguedes96.github.io/Landing-Page-Advogado/" */
-        link: "#"
+        link: "https://pauloguedes96.github.io/Landing-Page-Advogado/"
     },
     {
+        id:423423,
         nome: "Consulta CEP",
         description: "Projeto desenvolvido para que pessoas possam com facilidade consultar o cep de qualquer cidade do Brasil.",
         capa: "/assets/images/capa-2.jpg",
-        /*         link:"https://pauloguedes96.github.io/Consulta-CEP/" */
-        link: "#"
+        link: "https://pauloguedes96.github.io/Consulta-CEP/"
     },
     {
+        id:25345,
         nome: "Conversor de Moedas",
         description: "Projeto desenvolvido para que de maneira simplificada se possa fazer a conversão de moedas.",
         capa: "/assets/images/capa-3.jpg",
-        /*         link:"https://pauloguedes96.github.io/Conversor-de-Moedas/" */
-        link: "#"
+        link: "https://pauloguedes96.github.io/Conversor-de-Moedas/"
     },
     {
+        id:534534,
         nome: "CRUD",
         description: "CRUD criado com javascript, com fins de estudo, armazenando dados no local storage do navegador.",
         capa: "/assets/images/capa-4.jpg",
-        /*         link:"https://pauloguedes96.github.io/CRUD/" */
-        link: "#"
+        link: "https://pauloguedes96.github.io/CRUD/"
     }
 ];
 
@@ -40,12 +44,9 @@ function populaProjects() {
         titleProjeto[index].innerText = element.nome;
         descProjeto[index].innerHTML = element.description;
         headerProjeto[index].style.backgroundImage = `url('${element.capa}')`;
-        btnProjeto[index].href = element.link;
+        btnProjeto[index].id = element.id;
     });
 }
 
-document.addEventListener('DOMContentLoaded', () => {
 
-    populaProjects();
-});
 
--- scripts/modal.js
+++ scripts/modal.js
@@ -1,13 +1,32 @@
 let modal = document.querySelector('#modal');
 modal.style.display = 'none';
 
-function mostraModal() {
+let titleProjetoModal = document.querySelector('#titleProjetoModal');
+let descProjetoModal = document.querySelector('#descProjetoModal');
+let headerProjetoModal = document.querySelector('#headerProjetoModal');
+let btnProjetoModal = document.querySelector('#btnProjetoModal a');
+
+function mostraModal(e) {
 
     if (modal.style.display == 'none') {
         modal.style.display = 'flex';
+        populaModal(e.id)
     }
     else {
         modal.style.display = 'none';
     }
 }
 
+function populaModal(id) {
+
+    /* retorna somente o projeto clicado  */
+    let projClicado = projetos.filter((projeto) => { return projeto.id == id });
+
+    titleProjetoModal.innerHTML = projClicado[0].nome;
+
+    descProjetoModal.innerHTML = projClicado[0].description;
+
+    headerProjetoModal.style.backgroundImage = `url('${projClicado[0].capa}')`;
+
+    btnProjetoModal.href = projClicado[0].link;
+}
