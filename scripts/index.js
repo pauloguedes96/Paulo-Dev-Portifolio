709db5e1c5b9d93859388dec51d49308eeda0274
Paulo Guedes <cesarmota94@gmail.com>
2022-03-27 16:48:28 -0300

    c10

--- scripts/index.js
+++ scripts/index.js
@@ -12,28 +12,28 @@ let projetos = [
         id:1231231,
         nome: "Landind Page Advogado Online",
         description: "Landing page desenvolvida para advogados, com intuito de captar clientes e passar mais confiança aos mesmos.",
-        capa: "../assets/images/capa-1.jpg",
+        capa: "/assets/images/capa-1.jpg",
         link: "https://pauloguedes96.github.io/Landing-Page-Advogado/"
     },
     {
         id:423423,
         nome: "Consulta CEP",
         description: "Projeto desenvolvido para que pessoas possam com facilidade consultar o cep de qualquer cidade do Brasil.",
-        capa: "../assets/images/capa-2.jpg",
+        capa: "/assets/images/capa-2.jpg",
         link: "https://pauloguedes96.github.io/Consulta-CEP/"
     },
     {
         id:25345,
         nome: "Conversor de Moedas",
         description: "Projeto desenvolvido para que de maneira simplificada se possa fazer a conversão de moedas.",
-        capa: "../assets/images/capa-3.jpg",
+        capa: "/assets/images/capa-3.jpg",
         link: "https://pauloguedes96.github.io/Conversor-de-Moedas/"
     },
     {
         id:534534,
         nome: "CRUD",
         description: "CRUD criado com javascript, com fins de estudo, armazenando dados no local storage do navegador.",
-        capa: "../assets/images/capa-4.jpg",
+        capa: "/assets/images/capa-4.jpg",
         link: "https://pauloguedes96.github.io/CRUD/"
     }
 ];
