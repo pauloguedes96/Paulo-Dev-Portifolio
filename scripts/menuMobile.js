e179e926b6902f913b2bed1aaac11995622dd656
Paulo Guedes <cesarmota94@gmail.com>
2022-05-21 20:22:32 -0300

    c19

--- scripts/menuMobile.js
+++ scripts/menuMobile.js
@@ -1,10 +1,14 @@
-const btnMobile = document.getElementById("btnMobile");
+const btnMobile = document.querySelector("#btnMobile");
+const nav = document.getElementById("navHeader");
+const btnLinks = document.querySelectorAll(".linkMenu");
 
-//Evento clique menu mobile
-btnMobile.addEventListener("click", toggleMenu);
-
-//função que adiciona ou elimina classe active no elemento html
+//função que adiciona ou elimina classe active do menu
 function toggleMenu() {
-    const nav = document.getElementById("navHeader");
     nav.classList.toggle('active');
 }
+
+//Recolhe ou abre o menu ao clicar no botão menu
+btnMobile.addEventListener("click", toggleMenu);
+
+//Recolhe menu ao clicar nos items
+btnLinks.forEach((element) => element.addEventListener('click', toggleMenu));
